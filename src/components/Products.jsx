c4006b99cdff034adb5fdf5ade96e72f60739086
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-18 11:44:33 +0530

    product.css

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,9 @@
+import '../styles/Products.css'
 
 export const Products = () => {
     return (
-        <div>
-            
+        <div className='product' >
+            <h1>Products</h1>
         </div>
     )
 }
