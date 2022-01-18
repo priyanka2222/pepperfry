caabffcc103113c74be52247c3380b8f6539749b
maheshvaitla <maheshvaitla@gmail.com>
2022-01-18 23:15:12 +0530

    pagesg added

---
+++ src/components/CartAddress.jsx
@@ -0,0 +1,5 @@
+export const CartAddress = ()=>{
+    return (
+        <div></div>
+    )
+}
--- src/components/Single.jsx
+++
@@ -1,7 +0,0 @@
-export const Single = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
