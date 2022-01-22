f17bebcb3289c42f6301f68496cda2dbd4594041
maheshvaitla <maheshvaitla@gmail.com>
2022-01-22 17:12:31 +0530

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,13 @@
 
 import { HomeStatic } from "./HomeStatic"
 import Login from "./Login"
-import { Products } from "./Products"
-import { SingleProduct } from "./SingleProduct"
-import { Payment } from "./Payment"
-import { Cart } from "./Cart"
 import { Header } from "./Header"
-
 import {Footer} from './Footer'
 
 export const Home = () => {
     return (
         <div>
-            {/* <Payment /> */}
             <Header />
-          
             <HomeStatic />
             <Footer />
   
